895c4d7df2e888d7f5f675674bf51b7047812189
Gusthavo Silva <gusthavosilvabarreto123@gmail.com>
2025-11-01 18:52:47 -0300

    Incluindo JavaScript no Projeto.

---
+++ script.js
@@ -0,0 +1,49 @@
+prevButton = document.getElementById('prev')
+nextButton = document.getElementById('next')
+items = document.querySelectorAll('.item')
+dots = document.querySelectorAll('.dot')
+numberIndicator = document.querySelector('.numbers')
+list = document.querySelectorAll('.list')
+
+
+let active = 0;
+const total = items.length;
+let timer;
+
+function update(direction) {
+    document.querySelector('.item.active').classList.remove('active');
+    document.querySelector('.dot.active').classList.remove('active');
+
+    if (direction > 0) {
+        active = active + 1
+        if (active === total)
+            active = 0
+    }
+
+    else if (direction < 0) {
+        active = active - 1
+
+        if (active < 0 )
+            active = total - 1
+    }
+
+    items[active].classList.add('active');
+    dots[active].classList.add('active');
+
+    numberIndicator.textContent = String (active + 1).padStart(2, '0');
+}
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000)
+
+
+
+prevButton.addEventListener('click', () => {
+    update(-1);
+})
+
+nextButton.addEventListener('click', () => {
+    update(+1);
+})
